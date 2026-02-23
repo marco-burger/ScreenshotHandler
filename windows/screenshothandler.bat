2c11f3bb76b75df06afec11b4807f5853b920fd6
Marco Burger <127496020+marco-burger@users.noreply.github.com>
2026-02-23 06:37:27 +0100

    Update Script-Datei

--- windows/screenshothandler.bat
+++ windows/screenshothandler.bat
@@ -1,2 +1,5 @@
 @echo off
-"C:\Users\<USER>\AppData\Local\Programs\Python\Python314\python.exe" "C:\Users\<USER>\CronTabs\ScreenshotHandler\screenshothandler.py"
+set SCRIPT=C:\Users\<USER>\CronTabs\ScreenshotHandler\screenshothandler.py
+set LOG=C:\Users\<USER>\CronTabs\ScreenshotHandler\logs\run.log
+
+py -3.14 "%SCRIPT%" >> "%LOG%" 2>&1
